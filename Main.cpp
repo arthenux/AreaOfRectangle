eccb2300f819ba60f898eddb4b8bc061b4dce1b4
Alan <140322633+arthenux@users.noreply.github.com>
2025-03-15 15:36:57 +0000

    first commit

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length = 4;
+    int width = 6;
+    int area;
+
+    area = length * width;
+
+    cout << "Length is: " << length << endl;
+    cout << "Width is: " << width << endl;
+    cout << "Area of the rectangle is: " << area << endl;
+
+    return 0;
+}
